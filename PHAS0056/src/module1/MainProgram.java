d9c8ce5e82edfce12668481652f115877115cd0b
Callum Limbrick <zcaplim@ucl.ac.uk>
2019-09-30 15:13:22 +0100

    second commit because issue with first???

--- PHAS0056/src/module1/MainProgram.java
+++ PHAS0056/src/module1/MainProgram.java
@@ -1,9 +1,15 @@
 package module1;
 
+import java.util.Date;
+
 public class MainProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Program starting");
+		Date myDate = new Date();
+		System.out.println(myDate);
+		System.out.println("Program finished");
 
 	}
 
