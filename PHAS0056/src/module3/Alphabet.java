1e39a54dad1c6a61bbbfb070d3879440fe0765c9
Callum Limbrick <zcaplim@ucl.ac.uk>
2019-10-18 16:42:03 +0100

    Added random character generator

--- PHAS0056/src/module3/Alphabet.java
+++ PHAS0056/src/module3/Alphabet.java
@@ -1,18 +1,22 @@
 package module3;
+import java.util.Random;
 
 public class Alphabet {
+	
+	public static char randomCharacter() {
+		Random rand = new Random();
+		int n = rand.nextInt();
+		return (char) n;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
 		
-//	public static int randomCharacter() {
-//		
-//	}
-//	
-//	
-//	
-//	
-//	
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
+		for (int i = 0; i ==399; i++);
+			
+	}
 
 }
