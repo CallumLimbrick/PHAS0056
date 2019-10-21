32a0e3b4e056838e2bdcc48f2eee9467c97c21f2
Callum Limbrick <zcaplim@ucl.ac.uk>
2019-10-21 09:36:36 +0100

    Completed Alphabet file. To be submitted!!!

--- PHAS0056/src/module3/Alphabet.java
+++ PHAS0056/src/module3/Alphabet.java
@@ -1,22 +1,43 @@
 package module3;
 import java.util.Random;
+import java.lang.Character;
+import java.lang.StringBuilder;
+import java.lang.Integer;
 
 public class Alphabet {
 	
 	public static char randomCharacter() {
 		Random rand = new Random();
-		int n = rand.nextInt();
+		int n = rand.nextInt(127);
 		return (char) n;
-	}
-	
-	
-	
-	
+	}	
 	
 	public static void main(String[] args) {
 		
-		for (int i = 0; i ==399; i++);
+		int total = 0;
+		int except = 0;
+		
+		StringBuilder builder = new StringBuilder(400);
+		
+		for (int i = 0; i <= 399; i++) {
 			
+			char a = randomCharacter();
+			if (Character.isDigit(a) == true || Character.isLetter(a) == true) {
+				
+				builder.append(a);
+								try {
+					total += Integer.parseInt(String.valueOf(a));
+				}
+				catch (Exception e) {
+					except += 1;
+				}
+			}
+		}
+		
+		System.out.println(builder);
+		System.out.println("The length of the above string is: " + builder.length());
+		System.out.println("The sum of all the integers in the string is: " + total);
+		System.out.println("The total number of all letters in the string is: " + except);
 	}
 
 }
