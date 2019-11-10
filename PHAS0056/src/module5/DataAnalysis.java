5c7b484dedef7acbb3e4de44e4c4dff91c9e41b4
Callum Limbrick <zcaplim@ucl.ac.uk>
2019-11-10 17:34:58 +0000

    Completed and tested Minerals. Exceptions for Minerals and DataAnalysis

--- PHAS0056/src/module5/DataAnalysis.java
+++ PHAS0056/src/module5/DataAnalysis.java
@@ -7,9 +7,11 @@ import java.io.*;
 
 public class DataAnalysis {
 	
-	public static ArrayList<DataPoint> datafromURL(String url) throws IOException {
-		/*
-		 * 
+	public static ArrayList<DataPoint> dataFromURL(String url) throws Exception {
+		/* Retrieves data from URL and returns ArrayList of data
+		 * INPUT: url of data
+		 * OUTPUT: ArrayList of DataPoint objects
+		 * Throws exception if there are too many columns of data in any of the lines in the url
 		 */
 		URL u = new URL(url);
 		InputStream is = u.openStream();
@@ -17,11 +19,19 @@ public class DataAnalysis {
 		BufferedReader b = new BufferedReader(isr);
 		ArrayList<DataPoint> mypoints = new ArrayList<DataPoint>();
 		String line = "";
+		
 		while((line = b.readLine()) != null) {
 			Scanner s = new Scanner(line);
+			
 			double x = s.nextDouble();
 			double y = s.nextDouble();
 			double ey = s.nextDouble();
+			
+			if(s.hasNext()) {
+				s.close();
+				throw new Exception("Data not in correct form (too many columns)");
+			}
+			
 			DataPoint p = new DataPoint(x, y, ey);
 			mypoints.add(p);
 			s.close();
@@ -30,8 +40,9 @@ public class DataAnalysis {
 	}
 	
 	public static double goodnessOfFit(Theory n, ArrayList<DataPoint> data) {
-		/*
-		 * 
+		/* Runs calculation of CHI^2 by summing over all data and comparing to theoretical values
+		 * INPUT: Theory object to set n value, ArrayList of DataPoint Objects
+		 * OUTPUT: double CHI^2 value for dataset
 		 */
 		double chiSquared = 0;
 		for(int i=0; i<data.size();i++) {
@@ -45,27 +56,30 @@ public class DataAnalysis {
 		return chiSquared;
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
-		ArrayList<DataPoint> a = datafromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-xy.txt");
-		
-		double chiSquared2 = goodnessOfFit(new Theory(2), a);
-		double chiSquared4 = goodnessOfFit(new Theory(4), a);
-		
-		System.out.println("The chi squared value for the prediction y=x^2 is: " + chiSquared2);
-		System.out.println("The chi squared value for the prediction y=x^4 is: " + chiSquared4);
-		
-		if(chiSquared2<chiSquared4) {
-			System.out.println("y=x^2 is the better theoretical model to describe this data as it has a chi squared value of " + chiSquared2);
+		try {
+			ArrayList<DataPoint> a = dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-xy.txt");
+			
+			double chiSquared2 = goodnessOfFit(new Theory(2), a);
+			double chiSquared4 = goodnessOfFit(new Theory(4), a);
+			
+			System.out.println("The chi squared value for the prediction y=x^2 is: " + chiSquared2);
+			System.out.println("The chi squared value for the prediction y=x^4 is: " + chiSquared4);
+			
+			if(chiSquared2<chiSquared4) {
+				System.out.println("y=x^2 is the better theoretical model to describe this data as it has a chi squared value of " + chiSquared2);
+			}
+			else if(chiSquared2==chiSquared4) {
+				System.out.println("Both y=x^2 and y=x^4 describe the model equally well as they both have a chi squared value of "+ chiSquared2);
+			}
+			else {
+				System.out.println("y=x^4 is the better theoretical model to describe this data as it has a chi squared value of " + chiSquared4);
+			}			
 		}
-		else if(chiSquared2==chiSquared4) {
-			System.out.println("Both y=x^2 and y=x^4 describe the model equally well as they both have a chi squared value of "+ chiSquared2);
+		catch (Exception e) {
+			System.out.println(e);
 		}
-		else {
-			System.out.println("y=x^4 is the better theoretical model to describe this data as it has a chi squared value of " + chiSquared4);
-		}
-		
-		
 	}
 }
 
--- PHAS0056/src/module5/Minerals.java
+++ PHAS0056/src/module5/Minerals.java
@@ -1,10 +1,95 @@
 package module5;
 
+import java.io.*;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Minerals {
 
-	public static void main(String[] args) {
+	public static HashMap<Integer, String> dataFromURL(String url) throws Exception {
+		/* Retrieves data from url and returns it in HashMap
+		 * INPUT: url of data
+		 * OUTPUT: HashMap of keys (Integers) and values (Strings)
+		 * Throws exception if too many columns in dataset
+		 */
+		URL u = new URL(url);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader b = new BufferedReader(isr);
+		HashMap<Integer, String> data = new HashMap<Integer, String>();
+		String line = "";
 		
-
+		while((line = b.readLine()) != null) {
+			Scanner s = new Scanner(line);
+			int key;
+			String value;
+			// Checking if the key is the first or second number in the dataset
+			if(s.hasNextInt()) {
+				key = Integer.parseInt(s.next());
+				value = s.next();
+			}
+			else {
+				value = s.next();
+				key = Integer.parseInt(s.next());
+			}
+			if(s.hasNext()) {
+				s.close();
+				throw new Exception("Data not in correct form (too many columns)");
+			}
+			data.put(key, value);
+			s.close();
+		}
+		return data;
 	}
 
+	public static void main(String[] args) throws IOException {
+
+		try {
+			// Creating HashMaps for mass and location
+			HashMap<Integer, String> masses = dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-samples.txt");
+			HashMap<Integer, String> locations = dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-locations.txt");
+
+			// initialising max and min masses
+			double minMassVal = Double.MAX_VALUE;
+			double maxMassVal = 0;
+			int minMassKey = 0;
+			int maxMassKey = 0;
+
+			// Looping through HashMap to find maximum and minimum masses
+			for(int i : masses.keySet()) {
+				if(Double.parseDouble(masses.get(i))<minMassVal) {
+					minMassVal = Double.parseDouble(masses.get(i));
+				}
+				if(Double.parseDouble(masses.get(i))>maxMassVal) {
+					maxMassVal = Double.parseDouble(masses.get(i));
+				}
+			}
+			
+			//Retrieving keys for min amd max masses
+			for(int i : masses.keySet()) {
+				if(Double.parseDouble(masses.get(i)) == minMassVal) {
+					minMassKey = i;
+				}
+				if(Double.parseDouble(masses.get(i)) == maxMassVal) {
+					maxMassKey = i;
+				}	
+			}
+			
+			// throws exception if keys of min and max mass have not been updated
+			if(minMassKey == 0 || maxMassKey == 0) {
+				throw new Exception("Could not find corresponding locations for Min/Max masses");
+			}
+			
+			// Retrieving locations of min and max masses
+			String minMassLoc = locations.get(minMassKey);
+			String maxMassLoc = locations.get(maxMassKey);
+			
+			System.out.println(minMassKey + " has the minimum mass of " + minMassVal + ", it was found at " + minMassLoc);
+			System.out.println(maxMassKey + " has the maximimum mass of " + maxMassVal + ", it was found at " + maxMassLoc);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
