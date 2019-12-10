b52c58ae54e5031a78dd95c052c9f6a2dc6319ef
Callum Limbrick <zcaplim@ucl.ac.uk>
2019-12-10 11:36:17 +0000

    Runnable and constructor created

--- PHAS0056/src/module8/CountdownTask.java
+++ PHAS0056/src/module8/CountdownTask.java
@@ -1,5 +1,17 @@
 package module8;
 
-public class CountdownTask {
+public class CountdownTask implements Runnable {
+	
+	private int number;
+	
+	public CountdownTask(int number) {
+		this.number = number;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- PHAS0056/src/module8/PrimeNumberTask.java
+++ PHAS0056/src/module8/PrimeNumberTask.java
@@ -1,5 +1,11 @@
 package module8;
 
-public class PrimeNumberTask {
+public class PrimeNumberTask implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
