55fe8741993b0f71c7de9071985eee1caae2b8b7
Callum Limbrick <zcaplim@ucl.ac.uk>
2019-12-10 11:51:00 +0000

    inmplement CountdownTask.run()

--- PHAS0056/src/module8/CountdownTask.java
+++ PHAS0056/src/module8/CountdownTask.java
@@ -1,17 +1,30 @@
 package module8;
 
+/*
+ * 
+ */
 public class CountdownTask implements Runnable {
 	
 	private int number;
 	
+	/*
+	 * 
+	 */
 	public CountdownTask(int number) {
 		this.number = number;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		int i = number;
+		while (true) { // run until interrupted
+			if (Thread.currentThread().isInterrupted()) return;
+			System.out.print(i);
+			i -= 1;
+		}
 	}
-
 }
