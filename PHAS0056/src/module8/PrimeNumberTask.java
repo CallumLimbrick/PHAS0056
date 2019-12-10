09b736ed32f9d4673305e404a0bac73c8ab761f7
Callum Limbrick <zcaplim@ucl.ac.uk>
2019-12-10 12:19:49 +0000

    Run task created (incomplete)

--- PHAS0056/src/module8/PrimeNumberTask.java
+++ PHAS0056/src/module8/PrimeNumberTask.java
@@ -1,10 +1,21 @@
 package module8;
 
+import java.awt.List;
+
+/*
+ * 
+ */
 public class PrimeNumberTask implements Runnable {
 
+	List primes = new List();
+	
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
--- PHAS0056/src/module8/ThreadsMain.java
+++ PHAS0056/src/module8/ThreadsMain.java
@@ -3,7 +3,7 @@ package module8;
 public class ThreadsMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
